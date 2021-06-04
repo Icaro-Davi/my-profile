1491c4dd1f2094f189b0fa6aff129c59e6f1b266
raidou12 <icarodaviduarte@gmail.com>
2021-06-04 19:23:41 -0300

    fix any bugs

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Spin } from 'antd';
 import { AiOutlineLoading } from 'react-icons/ai';
 
 interface LoadingProps {
@@ -7,6 +6,7 @@ interface LoadingProps {
     circle?: boolean;
     hideLoadingIcon?: boolean;
     hideBackground?: boolean;
+    size?: number;
 }
 
 const Loading: React.FC<LoadingProps> = props => {
@@ -15,22 +15,25 @@ const Loading: React.FC<LoadingProps> = props => {
             {props.children}
         </React.Fragment>
     ) : (
-            <div style={{ position: 'relative' }}>
-                <div style={{ filter: 'blur(5px)' }}>
-                    {props.children}
-                </div>
-                <div
-                    className="nz-flex-row x-center y-center"
-                    style={{
-                        ...props.circle ? { borderRadius: '50%' } : {},
-                        position: 'absolute',
-                        top: 0, left: 0,
-                        width: '100%', height: '100%',
-                        background: props.hideBackground ? 'transparent' : '#00000033',
-                    }}
-                >{props.hideLoadingIcon ? '' : <AiOutlineLoading className="nz-spin" size={30} />}</div>
-            </div>
-        );
+        <div style={{ position: 'relative', ...props.size ? {width: props.size, height: props.size } : {} }}>
+            {React.Children.map(props.children, child =>
+                (React.isValidElement(child)) ? React.cloneElement(child, {
+                    ...child.props,
+                    style: { ...child.props?.style, filter: 'blur(5px)' },
+                }) : null)}
+            <div
+                className="nz-flex-row x-center y-center"
+                style={{
+                    ...props.circle ? { borderRadius: '50%' } : {},
+                    filter: 'blur(0px)',
+                    position: 'absolute',
+                    top: 0, left: 0,
+                    width: '100%', height: '100%',
+                    background: props.hideBackground ? 'transparent' : '#00000033',
+                }}
+            >{props.hideLoadingIcon ? '' : <AiOutlineLoading className="nz-spin" color="#FFFFFF" size={30} />}</div>
+        </div>
+    );
 }
 
 export default Loading;
