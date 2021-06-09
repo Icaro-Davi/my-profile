d7a56d746193f7083fd2f84d6be24463d026eea5
raidou12 <icarodaviduarte@gmail.com>
2021-06-09 17:58:22 -0300

    creating curriculum layout

--- components/UserCard/index.tsx
+++ components/UserCard/index.tsx
@@ -1,38 +1,33 @@
-import { Col, message, Row, Typography } from 'antd';
+import { message, Typography } from 'antd';
 import { useLayout } from '../../context/layout';
 import { useUser } from '../../hooks/useUser';
-import CustomDivider from '../Divider';
 import Loading from '../Loading';
 
-const { Title, Paragraph } = Typography;
+const { Title, } = Typography;
 
 const UserCard: React.FC = props => {
     const { locale } = useLayout();
     const { user, isLoading } = useUser('icaro-davi', () => {
         message.error(locale.userCardProfileErrorOnGetUser, 6);
     });
-    
+
     return (
-        <Row align='middle' gutter={[16, 8]}>
-            <Col xs={24}>
-                <div className={`nz-flex-row  y-center`}>
-                    <Loading circle isLoading={isLoading} size={100}>
-                        <picture style={{ width: 100, height: 100 }} title={`${user?.name}`}>
-                            <img style={{ width: 'inherit', height: "auto", borderRadius: "50%" }} alt={user?.name} src={user?.avatar_url} />
-                        </picture>
-                    </Loading>
-                    <Loading hideBackground hideLoadingIcon isLoading={isLoading}>
-                        <Title className='nz-padding-left-lg' level={2} title={locale.userCardProfileTitleDefaultTitle}>
-                            {locale.userCardProfileTitleMe} <b style={{ whiteSpace: 'nowrap' }}>{user?.name.replace(/([\w]*) ([\w]*) .*/g, "$1 $2")}</b>
-                        </Title>
-                    </Loading>
-                </div>
-            </Col>
-            <Col xs={24}>
-                <Paragraph className="nz-font-md nz-font-justify" title={locale.userCardProfileTitleDefaultTitle}>{locale?.userCardProfileBio}</Paragraph>
-                <CustomDivider />
-            </Col>
-        </Row>
+        <div className={`nz-flex-row  y-center`}>
+            <Loading circle isLoading={isLoading} size={100}>
+                <picture style={{ width: 100, height: 100 }} title={`${user?.name}`}>
+                    <img style={{
+                        width: 'inherit',
+                        height: "auto",
+                        borderRadius: "50%",                        
+                    }} alt={user?.name} src={user?.avatar_url} />
+                </picture>
+            </Loading>
+            <Loading hideBackground hideLoadingIcon isLoading={isLoading}>
+                <Title className='nz-padding-left-lg text' level={2} title={locale.userCardProfileTitleDefaultTitle}>
+                    {locale.userCardProfileTitleMe} <b style={{ whiteSpace: 'nowrap' }}>{user?.name.replace(/([\w]*) ([\w]*) .*/g, "$1 $2")}</b>
+                </Title>
+            </Loading>
+        </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,47 @@
 import { NextPage } from 'next';
+import { Col, Row, Typography } from 'antd';
+
 import UserCard from '../components/UserCard';
+import CustomDivider from '../components/Divider';
+import { useLayout } from '../context/layout';
+
+const { Paragraph, Text } = Typography;
 
 const App: NextPage = props => {
+    const { locale } = useLayout();
     return (
-        <>
-            <UserCard />
-        </>
+        <Row align='middle' gutter={[0, 8]}>
+            <Col xs={24}>
+                <CustomDivider orientation="left" >
+                    <Text className="nz-font-md">{locale.userCardProfileTitleDefaultTitle}</Text>
+                </CustomDivider>
+            </Col>
+            <Col>
+                <UserCard />
+            </Col>
+            <Col>
+                <Paragraph className="nz-font-md nz-font-justify" title={locale.userCardProfileTitleDefaultTitle}>{locale?.userCardProfileBio}</Paragraph>
+            </Col>
+            <Col xs={24}>
+                <CustomDivider orientation="left">
+                    <Text className="nz-font-md">Experiência</Text>
+                </CustomDivider>
+            </Col>
+            <Col>
+                <Paragraph className="nz-font-md nz-font-justify" title={locale.userCardProfileTitleDefaultTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae molestiae tempora nisi voluptates beatae, repudiandae itaque, vero, praesentium vel nam officiis. Doloremque esse iusto nobis iure odio a similique incidunt?</Paragraph>
+            </Col>
+            <Col xs={24}>
+                <CustomDivider orientation="left">
+                    <Text className="nz-font-md">Soft Skills</Text>
+                </CustomDivider>
+            </Col>
+            <Col>
+                <li className="nz-font-md">Lorem 1</li>
+                <li className="nz-font-md">Lorem 2</li>
+                <li className="nz-font-md">Lorem 3</li>
+                <li className="nz-font-md">Lorem 4</li>
+            </Col>
+        </Row >
     )
 };
 
