aff387da0a9a0949acc87790f7c00b7cf6497f48
raidou12 <icarodaviduarte@gmail.com>
2021-11-07 23:24:04 -0300

    added portfolio cards

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,27 +1,42 @@
-const generateboxs = (numBox = 10) => {
-    const elements = [];
-    for (let index = 0; index < numBox; index++) {
-        elements.push(
-            <div
-                key={index}
-                style={{
-                    height: 50,
-                    width: 50,
-                    backgroundColor: '#FFFFFF',
-                    marginBottom: 6
-                }}
-            >{index + 1}</div>
-        );
-    }
-    return elements;
-}
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { NextPage } from 'next';
 
-const Portfolio = () => {
+import { CardList } from '../components/Portfolio/Card';
+import { useLayout } from '../context/layout';
+
+const Portfolio: NextPage = () => {
+    const { locale } = useLayout();
     return (
-        <>
-            {generateboxs(5)}
-        </>
-    )
+        <Fragment>
+            <Head>
+                <title>{locale.portfolio.name}</title>
+            </Head>
+            <CardList
+                title="Projetos que já participei"
+                cards={[
+                    {
+                        title: "Mìticard (2020)",
+                        href: "https://miticard.com.br",
+                        pictureSrc: "/portfolio/miticard.jpg",
+                        description: "https://miticard.com.br"
+                    },
+                    {
+                        title: "Mostruário (2020)",
+                        href: "https://site.mostruar.io",
+                        pictureSrc: "/portfolio/mostruario.jpg",
+                        description: "Cadastre sua loja e venda online seus produtos."
+                    },
+                    {
+                        title: "Mallbuyle (2021)",
+                        href: "https://play.google.com/store/apps/details?id=br.com.mallbuyle",
+                        pictureSrc: "/portfolio/mallbuyle.jpg",
+                        description: 'App de vendas de produtos por região.'
+                    },
+                ]}
+            />
+        </Fragment>
+    );
 };
 
 export default Portfolio;
