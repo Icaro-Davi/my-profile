c0f97e852c49a0e053b6bc56fe3627af70de0dc2
raidou12 <icarodaviduarte@gmail.com>
2021-11-07 23:20:21 -0300

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../assets/styles/Global.less';
+import React from 'react';
 import { AppContext, AppProps } from 'next/app';
 
 import { LayoutProvider } from '../context/layout';
@@ -9,17 +10,19 @@ import rootsConfig from '../assets/rootsConfig.json';
 import handleLocale from '../utils/handleLocale';
 
 interface InitialAppProps extends AppProps {
-  layoutProps: LayoutInitialProps
+  layoutProps: LayoutInitialProps;
 }
 
-const MyApp: React.FC<InitialAppProps> = ({ Component, layoutProps, pageProps }) => (
-  <LayoutProvider {...{ ...layoutProps }}>
-    <Component {...pageProps} />
-  </LayoutProvider>
+const MyApp: React.FC<InitialAppProps> = ({ Component, layoutProps, pageProps: { head, ...pageProps } }) => (
+  <React.Fragment>
+    <LayoutProvider {...{ ...layoutProps }}>
+      <Component {...pageProps} />
+    </LayoutProvider>
+  </React.Fragment>
 );
 
 MyApp['getInitialProps'] = async ({ Component, ctx }: AppContext) => {
-  const pageProps = (Component.getInitialProps ? Component.getInitialProps(ctx) : {});
+  const pageProps = (Component.getInitialProps ? await Component.getInitialProps(ctx) : {} as any);
   const layoutProps: LayoutInitialProps = {};
   if (!process.browser) {
     layoutProps.themeType = getAppThemeType(ctx);
